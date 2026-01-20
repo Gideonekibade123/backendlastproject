5a00ad78bb008001414e46904b217b3188690afb
Gideon <ekibade.gideon@yahoo.comm>
2026-01-20 23:51:06 +0100

    Because of the added domain name

--- realestate/settings.py
+++ realestate/settings.py
@@ -27,7 +27,8 @@ SECRET_KEY = 'django-insecure-6njj80&q2@=vux7e_rn#2&4ms(all8s_%b4^9y=s!@8#4m5mv!
 
 DEBUG = False 
 
-ALLOWED_HOSTS = ["https://backendlastproject.onrender.com"]
+ALLOWED_HOSTS = [ "backendlastproject.onrender.com",
+    "backendlastproject-1.onrender.com",]
 
 
 # Application definition
