c7c02c9c3fe1c02c54b75cd4903caa7df5804198
Gideon <ekibade.gideon@yahoo.comm>
2026-02-03 22:26:17 +0100

    Adding to .env

--- realestate/settings.py
+++ realestate/settings.py
@@ -209,7 +209,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'gekibade@gmail.com'
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
-#EMAIL_HOST_PASSWORD =   'lczw bsct hfpb ufth'    #Geki12345@ 
+#EMAIL_HOST_PASSWORD =   
 EMAIL_PORT = '587'
 EMAIL_USE_TLS =True
 
