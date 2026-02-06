fbc22a3ccb2d80e1669efb921c566c52c42c6fbd
Gideon <ekibade.gideon@yahoo.comm>
2026-02-06 11:50:45 +0100

    Webhook updated

--- payments/urls.py
+++ payments/urls.py
@@ -1,21 +1,32 @@
 
-from django.urls import path
-from .views import PaystackInitializeAPIView, PaystackVerifyAPIView
+# from django.urls import path
+# from .views import PaystackInitializeAPIView, PaystackVerifyAPIView
+
+# # urlpatterns = [
+# #     path("paystack/init/", PaystackInitializeAPIView.as_view(), name="paystack-init"),
+
+
+# #     path("paystack/verify/<str:reference>/", PaystackVerifyAPIView.as_view(), name="paystack-verify"),
+
+# #    path("webhook/", PaystackVerifyAPIView.as_view(), name="paystack-webhook"),
+
+# # ]
+
 
 # urlpatterns = [
 #     path("paystack/init/", PaystackInitializeAPIView.as_view(), name="paystack-init"),
-
-
 #     path("paystack/verify/<str:reference>/", PaystackVerifyAPIView.as_view(), name="paystack-verify"),
-
-#    path("webhook/", PaystackVerifyAPIView.as_view(), name="paystack-webhook"),
-
+#     path("webhook/", PaystackWebhookAPIView.as_view(), name="paystack-webhook"),
 # ]
 
+
+
+from django.urls import path
+from .views import PaystackInitializeAPIView, PaystackVerifyAPIView, PaystackWebhookAPIView
 
 urlpatterns = [
     path("paystack/init/", PaystackInitializeAPIView.as_view(), name="paystack-init"),
     path("paystack/verify/<str:reference>/", PaystackVerifyAPIView.as_view(), name="paystack-verify"),
-    path("webhook/", PaystackVerifyAPIView.as_view(), name="paystack-webhook"),
+    path("paystack/webhook/", PaystackWebhookAPIView.as_view(), name="paystack-webhook"),  # ✅ Use webhook view
 ]
 
